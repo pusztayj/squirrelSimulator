20ca8d0f2b1999437f5ce20efb582e86cd098e57
Trevor <stalnakert20@mail.wlu.edu>
2019-11-10 14:14:40 -0500

    Removed main_copy and relocated mainlevel

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 
 import pygame, random, math
-from graphics.banner import Banner
-from graphics.textbox import TextBox
-from graphics.textinput import TextInput
-from graphics.popup import Popup
-from graphics.progressbar import ProgressBar
-from graphics.statdisplay import StatDisplay
-from graphics.mask import Mask
-from modules.vector2D import Vector2
-from modules.drawable import Drawable
+from minigame.mainlevel import MainLevel
 from player import Player
-from economy.acorn import Acorn
-from economy.dirtpile import DirtPile
-from minigame.atm import ATM
-from animals.chipmunk import Chipmunk
-from animals.turtle import Turtle
-from minigame.interaction import Interaction
+from minigame.merchantlevel import MerchantLevel
 
 SCREEN_SIZE = (1200,500)
-WORLD_SIZE  = (2000,500)
 
-def setPopup(lyst, mouse_pos, popup_pos, font):
-   for entity in lyst:
-      x,y = entity.getPosition()
-      for rect in entity.getCollideRects():
-         r = rect.move(x,y)
-         if r.collidepoint(mouse_pos):
-            return Popup(entity.getName(), popup_pos, font)
-               
 def main():
    """
    Main loop for the program
@@ -36,105 +14,34 @@ def main():
    pygame.init()
    pygame.font.init()
 
-   font = pygame.font.SysFont("Times New Roman", 32)
-   popupFont = pygame.font.SysFont("Times New Roman", 16)
-
-   hour_length = 1 # ticks / seconds
-   day_length = 24 * hour_length
-   minute_length = hour_length / 60
-
-   #Store the pygame key codes for simplicity later
-   movement_keys = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
-
    #Update the title for the window
    pygame.display.set_caption('Squirrel Simulator')
    
    #Get the screen
    screen = pygame.display.set_mode(SCREEN_SIZE)
 
-   #Load the background image
-   background = Drawable("background.png", Vector2(0,0))
-
    #Create an instance of the game clock
    gameClock = pygame.time.Clock()
 
-   player = Player(pos=(600,300))
-   player.scale(2)
-
-   ground = Banner((0,0),(100,255,100),(WORLD_SIZE[1],WORLD_SIZE[0]))
-
-   acorns = []
-   dirtPiles = []
-
-   acornSpawnTimer = random.randint(5,10)
-
-   hungerTimer = 2 * hour_length
-
-   starveTimer = 2 * hour_length
-
-   time = 0
-
-   popup = None #Popup("Pop", (0,0), popupFont)
-
-   stats = StatDisplay((5,5),player)
-
-   nightFilter = Mask((0,0),(1200,500),(20,20,50),150)
-
-   txtDay = TextBox("Day: 1", (500,5), font, (255,255,255))
-   txtHour = TextBox("12:00pm", (600,5), font, (255,255,255))
-   
-   creatures = []
-   chip = Chipmunk(pos=(1600,300))
-   chip.flip()
-   creatures.append(chip)
-   creatures.append(player)
-
-   atm = None
-
-   interaction = None
-
-   merchant = Turtle(pos=(random.randint(0,WORLD_SIZE[0]),random.randint(0,WORLD_SIZE[1])))
+   player = Player()
+   player.scale(1.5)
+   level = MainLevel(player, SCREEN_SIZE)
+   merchantLevel = None
 
    RUNNING = True
+
+   code = None
 
    while RUNNING:
 
       #Increment the clock
       gameClock.tick()
 
-      #Draw the background to the screen
-      background.draw(screen)
-      ground.draw(screen)
+      if level.isActive():
+          level.draw(screen)
 
-      for acorn in acorns:
-         acorn.draw(screen)
-
-      for pile in dirtPiles:
-         pile.draw(screen)
-
-      #Draw the player to the screen
-      player.draw(screen)
-
-      for creature in creatures:
-         creature.draw(screen)
-
-      merchant.draw(screen)
-
-      nightFilter.draw(screen)
-
-      if popup != None:
-         popup.draw(screen)
-
-      stats.draw(screen)
-
-      if atm != None and atm.getDisplay():
-         atm.draw(screen)
-
-      if interaction != None and interaction.getDisplay():
-         interaction.draw(screen)
-
-      txtDay.draw(screen)
-      txtHour.draw(screen)
+      if merchantLevel != None and merchantLevel.isActive():
+          merchantLevel.draw(screen)
 
       pygame.display.flip()
 
@@ -145,111 +52,36 @@ def main():
              (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             # change the value to False, to exit the main loop
             RUNNING = False
-            
-         player.move(event)
 
-         if (event.type == pygame.KEYDOWN and event.key == pygame.K_b):
-            if player.getAcorns() > 0:
-                  dp = DirtPile((player.getX() + (player.getWidth() // 2),
-                                      player.getY() + (player.getHeight() // 2)))
-                  dp.addAcorn()
-                  player.setAcorns(player.getAcorns() - 1)
-                  dirtPiles.append(dp)
+         if event.type == pygame.KEYDOWN and event.key == pygame.K_p and \
+            (level._atm == None or not level._atm.getDisplay()):
+            level.setActive(not level.isActive())
 
-         if event.type == pygame.MOUSEBUTTONDOWN and event.button==1:
-            for pile in dirtPiles:
-               if pile.getCollideRect().collidepoint((event.pos[0] + Drawable.WINDOW_OFFSET[0],
-                                                  event.pos[1] + Drawable.WINDOW_OFFSET[1])):
-                  atm = ATM(player, pile)
-                  pile.handleEvent(event, player)
-            for creature in creatures:
-               x,y = creature.getPosition()
-               for rect in creature.getCollideRects():
-                  r = rect.move(x,y)
-                  if r.collidepoint((event.pos[0] + Drawable.WINDOW_OFFSET[0],
-                                 event.pos[1] + Drawable.WINDOW_OFFSET[1])):
-                     interaction = Interaction()
-            
-         if atm != None and atm.getDisplay():
-            atm.handleEvent(event)
+         if level.isActive():
+             code = level.handleEvent(event)
 
-         if interaction != None and interaction.getDisplay():
-            interaction.handleEvent(event)
-            
-         # Button inputs to be used for testing
-         if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            player.loseHealth(10)
-         if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-            player.heal(10)
-         if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-            player.setAcorns(player.getAcorns() + 1)
-         if event.type == pygame.KEYDOWN and event.key == pygame.K_n:
-            if nightFilter.getAlpha() == 0:
-               nightFilter.setAlpha(150)
-            else:
-               nightFilter.setAlpha(0)
+         if merchantLevel != None and merchantLevel.isActive():
+             code = merchantLevel.handleEvent(event)
 
-         mouse = pygame.mouse.get_pos()
-         m_pos_offset = player.adjustMousePos(mouse)
-         m_pos_offset = (m_pos_offset[0], m_pos_offset[1])   
-         popup_pos = (mouse[0] + 5, mouse[1] + 5)
-         popup = setPopup(creatures, m_pos_offset, popup_pos, popupFont)
-         if popup==None:
-            popup = setPopup(dirtPiles, m_pos_offset, popup_pos, popupFont)
-            
-      for acorn in acorns:
-         if acorn.getCollideRect().colliderect(player.getCollideRect()) and \
-            player.getCheekCapacity() - player.getAcorns() > 0:
-            player.setAcorns(player.getAcorns()+1)
-            acorn.collected()    
-            
       #Calculate ticks
       ticks = gameClock.get_time() / 1000
 
-      acornSpawnTimer -= ticks
-      if acornSpawnTimer <= 0:
-         acorns.append(Acorn((random.randint(0,WORLD_SIZE[0]),random.randint(0,WORLD_SIZE[1]))))
-         acornSpawnTimer = random.randint(5,10)
+      # Set Game Mode to Merchant
+      if code != None and code[0] == 1:
+          level.setActive(False)
+          merch = code[1]
+          merchantLevel = MerchantLevel(player, merch, SCREEN_SIZE)
+          code = None
+      elif code != None and code[0] == 0:
+          level.setActive(True)
+          code = None
+        
+      if level.isActive():
+          level.update(ticks)
 
-      hungerTimer -= ticks
-      if hungerTimer <= 0:
-         player.decrementHunger()
-         hungerTimer = 2 * hour_length
-
-      time += ticks
-
-      nightFilter.setAlpha(math.sin(time/(day_length/4))*200)
-
-      if player.isStarving():
-         starveTimer -= ticks
-         if starveTimer <= 0:
-            player.loseHealth(5)
-            player.loseStamina(5)
-            starveTimer = 2 * hour_length
-      else:
-         starveTimer = 2 * hour_length
-
-      if player.isDead():
-         print("The Player is Dead")
-
-      #Update the player's position
-      player.update(WORLD_SIZE, ticks)
-
-      # Update the players stats
-      stats.update()
-      
-      #Update the offset based on the player's location
-      player.updateOffset(player, SCREEN_SIZE, WORLD_SIZE)
-
-      # Remove acorns from the world that have been collected
-      acorns = [acorn for acorn in acorns if not acorn.isCollected()]
-
-      #Update InGame Clock
-      txtDay.setText("Day: " + str(int(time // day_length)+1))
-      txtHour.setText(str(int((time // hour_length) % 12)+1) + ":" +
-                      str(int(time // minute_length)%60).zfill(2) + " " +
-                      ("pm" if (int((time // hour_length) % 24)+1 >= 12) else "am"))
-
+      if merchantLevel != None and merchantLevel.isActive():
+          merchantLevel.update(ticks)
+                   
    #Close the pygame window and quit pygame
    pygame.quit()
 
