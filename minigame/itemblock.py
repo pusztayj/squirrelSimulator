897ede0c65757414007825544e2f5d00da07e020
Trevor <stalnakert20@mail.wlu.edu>
2019-11-13 16:26:44 -0500

    Factored out the item block class

---
+++ minigame/itemblock.py
@@ -0,0 +1,43 @@
+import pygame
+from modules.drawable import Drawable
+
+class ItemBlock(Drawable):
+
+    def __init__(self, pos, dimensions, item=None, selected=False):
+        super().__init__("", pos, worldBound = False)
+        self._item = item
+        self._width = dimensions[0]
+        self._height = dimensions[1]
+
+        self._borderColor = (0,0,0)
+        self._selectedBorder = (150,150,150)
+        self._backgroundColor = (100,100,100)
+        self._borderWidth = 1
+
+        self._transparency = 200
+
+        self._selected = selected
+
+        self.updateBlock()
+
+    def updateBlock(self):
+        surfBack = pygame.Surface((self._width, self._height))
+        if self._selected:
+            surfBack.fill(self._selectedBorder)
+        else:
+            surfBack.fill(self._borderColor)
+        surfBack.set_alpha(self._transparency)
+
+        # Draw the background
+        surf = pygame.Surface((self._width - (self._borderWidth * 2),
+                              self._height - (self._borderWidth * 2)))
+        surf.fill(self._backgroundColor)
+        surf.set_alpha(self._transparency)
+
+        if self._item != None:
+            image = self._item.getImage()
+            surf.blit(image,(self._width//2 - self._item.getWidth()//2,
+                             self._height//2 - self._item.getHeight()//2))
+        
+        surfBack.blit(surf, (self._borderWidth, self._borderWidth))
+        self._image = surfBack
--- minigame/itemselect.py
+++ minigame/itemselect.py
@@ -3,6 +3,7 @@ import pygame
 from modules.drawable import Drawable
 from modules.frameManager import FRAMES
 from items.items import Spear
+from .itemblock import ItemBlock
 
 class ItemSelect(Drawable):
 
@@ -58,45 +59,3 @@ class ItemSelect(Drawable):
         # Draw the background
         for block in self._blocks:
             block.draw(screen)
-
-
-class ItemBlock(Drawable):
-
-    def __init__(self, pos, dimensions, item=None, selected=False):
-        super().__init__("", pos, worldBound = False)
-        self._item = item
-        self._width = dimensions[0]
-        self._height = dimensions[1]
-
-        self._borderColor = (0,0,0)
-        self._selectedBorder = (150,150,150)
-        self._backgroundColor = (100,100,100)
-        self._borderWidth = 1
-
-        self._transparency = 200
-
-        self._selected = selected
-
-        self.updateBlock()
-
-    def updateBlock(self):
-        surfBack = pygame.Surface((self._width, self._height))
-        if self._selected:
-            surfBack.fill(self._selectedBorder)
-        else:
-            surfBack.fill(self._borderColor)
-        surfBack.set_alpha(self._transparency)
-
-        # Draw the background
-        surf = pygame.Surface((self._width - (self._borderWidth * 2),
-                              self._height - (self._borderWidth * 2)))
-        surf.fill(self._backgroundColor)
-        surf.set_alpha(self._transparency)
-
-        if self._item != None:
-            image = self._item.getImage()
-            surf.blit(image,(self._width//2 - self._item.getWidth()//2,
-                             self._height//2 - self._item.getHeight()//2))
-        
-        surfBack.blit(surf, (self._borderWidth, self._borderWidth))
-        self._image = surfBack
