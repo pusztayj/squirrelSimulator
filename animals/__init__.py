c077f034054442e5d4692d0ac05e91354d306687
jpusztay <pusztayj20@mail.wlu.edu>
2019-11-16 21:45:29 -0500

    More packaging

---
+++ animals/__init__.py
@@ -0,0 +1,18 @@
+__all__ = ["Animal","Bear","Beaver","Chipmunk","Cow","Deer","Fox","HedgeHog",
+           "NPC","Pack","Rabbit","Shmoo","Snake","Squirrel","Turtle"]
+
+from .animal import Animal
+from .bear import Bear
+from .beaver import Beaver
+from .chipmunk import Chipmunk
+from .cow import Cow
+from .deer import Deer
+from .fox import Fox
+from .hedgehog import HedgeHog
+from .npc import NPC
+from .pack import Pack
+from .rabbit import Rabbit
+from .shmoo import Shmoo
+from .snake import Snake
+from .squirrel import Squirrel
+from .turtle import Turtle
