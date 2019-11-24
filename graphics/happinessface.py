67740a0b7572991cfd2d46e94c883cbe130824db
Trevor <stalnakert20@mail.wlu.edu>
2019-11-24 18:05:31 -0500

    code for the happyface display

---
+++ graphics/happinessface.py
@@ -0,0 +1,22 @@
+
+from modules.drawable import Drawable
+from modules.frameManager import FRAMES
+import pygame
+
+class HappinessFace(Drawable):
+
+    def __init__(self, pos):
+
+        Drawable.__init__(self, "face_0.png", pos, worldBound=False)
+
+    def setFace(self, happyStat):
+        if 0 <= happyStat < 20:
+            self._image = FRAMES.getFrame("face_4.png")
+        elif 20 <= happyStat < 40:
+            self._image = FRAMES.getFrame("face_3.png")
+        elif 40 <= happyStat < 60:
+            self._image = FRAMES.getFrame("face_2.png")
+        elif 60 <= happyStat < 80:
+            self._image = FRAMES.getFrame("face_1.png")
+        else:
+            self._image = FRAMES.getFrame("face_0.png")
