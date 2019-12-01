193108824d982ca735b2cdfd615c4336fe41ddf9
jpusztay <pusztayj20@mail.wlu.edu>
2019-12-01 16:49:19 -0500

    Fixed bug of not being able to retreat with no items

--- minigame/combatUtils.py
+++ minigame/combatUtils.py
@@ -137,7 +137,7 @@ def retreatItemLost(animal):
     """
     Chance of item lost from retreating from battle.
     """
-    if 10 <= random.randint(0,100):
+    if 10 <= random.randint(0,100) and len(animal.getInventory()) != 0:
         item = random.choice(animal.getInventory())
         return item
     else:
