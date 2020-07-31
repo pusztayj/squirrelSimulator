af17a55c7f099f409c519454474902e9065c7e86
Trevor <stalnakert20@mail.wlu.edu>
2020-07-31 10:57:38 -0400

    Removed the modules folder from SQS

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["Animated","Drawable","Mobile",
-            "Vector2"] 
-
-from .animated import Animated
-from .drawable import Drawable
-from .mobile import Mobile
-from .vector2D import Vector2
