71607001e39707d56e8a60b7f77deeeb579cc6f5
jpusztay <pusztayj20@mail.wlu.edu>
2020-11-14 22:43:24 +0000

    Begins adding how to change durability

--- minigame/combat/utils/combatfunctions.py
+++ minigame/combat/utils/combatfunctions.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 
 attackDamage = {(0.0,1):0,(0.25,1): 5, (0.5,1): 10, (0.75,1): 20, (1,1): 30,
                 (1.25,1): 33, (1.5,1): 35, (1.75,1): 40, (2,1): 45, (2.25,1): 47,
@@ -17,7 +20,22 @@ def attack(attacker, defender):
     # potential gain xp
     # potential stamina loss
 
+def deltaDurability(ratio,item, padding = 0.2):
+    """
+    Calculates change in durability among items from combat use
+    """
+    decayWeight = item.getAttribute("decayWeight")
+    if item.getAttribute("type") in ["weapon","tool"]:
+        return decayWeight/(ratio+padding) # inverse relationship 1/x
+    if item.getAttribute("type") == "armor":
+        return decayWeight/((3-ratio)+padding) # inverse relationship 1/x
+    return 0
 
+def randomDurabilityChange(deltaDurability):
+    mean = deltaDurability
+    std = 0.46875
+    return random.gauss(mean,std)
+    
 def attackComputation(attacker,defender):
     """
     Calculates the damage dealt between an attacker and a defender.
@@ -26,7 +44,7 @@ def attackComputation(attacker,defender):
     if attacker.getEquipItem() != None:
         attack_strength = (attacker.getStrength() + \
                                (attacker.getEquipItem().getAttribute("strength"))) * \
-                               attacker.getAttackModifers()
+                               attacker.getAttackModifers()        
     else:
         attack_strength = attacker.getStrength()*attacker.getAttackModifers()
 
@@ -38,6 +56,21 @@ def attackComputation(attacker,defender):
         defense_strength = defender.getStrength()*defender.getDefenseModifers()
                          
     ratio = (round((attack_strength/defense_strength) * 4))/ 4
+    if attacker.isEquipped():
+        current_durability = attacker.getEquipItem().getAttribute("durability")
+        print("Attacker Current Durability: ",current_durability)
+        delta = deltaDurability(ratio,attacker.getEquipItem())
+        new_durability = current_durability - delta + randomDurabilityChange(delta)
+        print("Attacker Durability: ",new_durability)
+        attacker.getEquipItem().setDurability(max(0,new_durability))
+    if defender.hasArmor():
+        current_durability = defender.getArmor().getAttribute("durability")
+        print("Defender Current Durability: ",current_durability)
+        delta = deltaDurability(ratio,attacker.getArmor())
+        new_durability = current_durability - delta + randomDurabilityChange(delta)
+        defender.getArmor().setDurability(max(0,new_durability))
+        print("Defender Durability: ",new_durability)
+                
     if ratio > 3:
         damage = 95
     else:
